184a9b4cba06f4096b9fa38eef5f992b4fe8f0fa
Pedro Furigo <pedrofurigo.dev@hotmail.com>
2025-11-10 17:48:55 -0300

    Solução do back end implementando todas as funcionalidades swagger

--- BackEnd/NexFinance/obj/Debug/net8.0/NexFinance.AssemblyInfo.cs
+++ BackEnd/NexFinance/obj/Debug/net8.0/NexFinance.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexFinance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98599207c1bc1a5b306d6e386f3cf848d8ea2551")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2555442c93f3562b5d2f9457bfd5471d617037be")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexFinance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexFinance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
