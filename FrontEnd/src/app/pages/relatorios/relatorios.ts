0357eb5ab838b98f62b4c68e4f9214ba6e41c7d5
Skinkarambola <joaogabrielesquinca@gmail.com>
2025-11-14 09:35:25 -0300

    Página de relatórios

--- FrontEnd/src/app/pages/relatorios/relatorios.ts
+++ FrontEnd/src/app/pages/relatorios/relatorios.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { PoPageModule, PoWidgetModule } from '@po-ui/ng-components';
+import { CommonModule } from '@angular/common';
+import { PoChartModule, PoChartSerie, PoChartType } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-relatorios',
-  imports: [
-    PoPageModule,
-    PoWidgetModule
-  ],
+  standalone: true,
+  imports: [CommonModule, PoChartModule],
   templateUrl: './relatorios.html',
-  styleUrl: './relatorios.css'
+  styleUrls: ['./relatorios.css']
 })
 export class RelatoriosComponent {
-  title = 'Relatórios';
+
+  lastUpdate = new Date();
+
+  categories = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun'];
+
+  
+
+  series: PoChartSerie[] = [
+    { label: 'Receitas', data: [1200, 1500, 900, 1800, 2200, 2000] },
+    { label: 'Despesas', data: [800, 1100, 700, 1200, 1500, 1300] }
+  ];
+
+   
+
+
 }
